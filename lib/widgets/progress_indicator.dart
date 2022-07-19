c753151a74e3929ce1a379fb501bc6f2365f993a
inlevkov <io.levk@gmail.com>
2022-07-19 13:00:54 +0800

    Release

---
+++ lib/widgets/progress_indicator.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ShowBelksProgressIndicator extends AnimatedWidget {
+  const ShowBelksProgressIndicator(
+      {required AnimationController animationController})
+      : super(
+            listenable:
+                animationController); //тут можно прописать Tween и далее импортировать анимацию
+
+  @override
+  Widget build(BuildContext context) {
+    AnimationController animation = listenable as AnimationController;
+
+    return Stack(
+      children: [
+        Image.asset(
+          'assets/nut-256.png',
+          height: 48,
+          width: 48,
+        ),
+        Image.asset(
+          'assets/nut-256green.png',
+          height: 48 - 48 * animation.value,
+          width: 48,
+          fit: BoxFit.fitWidth,
+          alignment: Alignment.topCenter,
+        ),
+      ],
+    );
+  }
+}
